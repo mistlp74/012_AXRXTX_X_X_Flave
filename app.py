4bd0acb9a5fbdc337b72d2f9cc7042498223940b
mistlp74 <dmytrobryzhatiuk@gmail.com>
2026-02-19 02:03:30 +0100

    upd001 //new pages

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, jsonify, render_template, request, redirect, url_for
 from User_verify import user_login
 
 app = Flask(__name__)
@@ -6,7 +6,6 @@ app = Flask(__name__)
 
 @app.route("/", methods=['POST', 'GET'])
 def starter_page():
-
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
@@ -20,7 +19,6 @@ def starter_page():
 
 
 
-
 @app.route("/Register")
 def register():
     return render_template("register.html")
@@ -29,6 +27,13 @@ def register():
 @app.route("/main")
 def main():
     return render_template("main.html")
+
+
+with app.test_request_context():
+    print(url_for('starter_page'))
+    print(url_for('register'))
+    print(url_for('main'))
+
 
 
 if __name__ == "__main__":
